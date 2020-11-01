f1f4deea7af5a18d7743b357253cc28200363658
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2020-11-01 17:58:06 -0300

    App.js reorder

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import React from "react";
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
-function App() {
-  return (
-    <div>
-      <Layout>
-        <BurgerBuilder />
-      </Layout>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <Layout>
+      <BurgerBuilder />
+    </Layout>
+  </div>
+);
 
 export default App;
