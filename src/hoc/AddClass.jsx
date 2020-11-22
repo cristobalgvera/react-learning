1533f18b5e03defb629daeed86508ec78ea9e630
Cristobal Gajardo Vera <cristobalgajardo.v@gmail.com>
2020-11-22 03:27:46 -0300

    nextjs-learning branch init

--- src/hoc/AddClass.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const AddClass = (WrappedComponent, className) => (props) => (
-  <div className={className}>
-    <WrappedComponent {...props} />
-  </div>
-);
-
-export default AddClass;
