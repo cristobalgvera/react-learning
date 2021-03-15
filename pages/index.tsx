bd47deeb2dd286ed48601eb78b731d01c5c1b095
Cristobal Gajardo Vera <cristobalgajardo.v@gmail.com>
2021-03-15 03:04:13 -0300

    TypeScript & SCSS support added

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Home.module.scss'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>Hello World from NextJS with TypeScript & SCSS support!</h1>
+    </div>
+  )
+}
